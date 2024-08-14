b4b4f992fda4489b806da4fee81c988ae8a47c99
llyogesh123 <yogeshwaranjith2004@gmail.com>
2024-08-14 19:37:51 +0530

    donate

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const { User, validate } = require("../models/user");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 require("dotenv").config()
 
 router.post("/", async (req, res) => {
